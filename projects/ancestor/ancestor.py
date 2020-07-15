a7ffe36992562e02f957a05e31d3fd9b268b0cfd
AustinPotts <pottsevolvements@gmail.com>
2020-07-15 12:24:17 -0400

    .

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -48,8 +48,9 @@ def earliest_ancestor(ancestors, starting_node):
 
 
 
-# We need a base case 
-
+# What is our node? When does our node have a connection?
+# Nodes are people 
+# Nodes are connected when th
 
 
 
